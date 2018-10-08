209eccedf3c43e3d5c41728a3c8b9d80c981afd2
AnXPyDev <jkism332@gmail.com>
2018-10-08 22:55:19 +0200

    The solution for B/2 works correctly

--- B/2/main.py
+++ B/2/main.py
@@ -1,9 +1,9 @@
 from sys import argv
 
 file = open("./input/" + argv[1], "r").read().split('\n')
-del file[0]
 
 words = []
+numberOfWords = int(file[0])
 
 class Word:
     def __init__(self, word, index):
@@ -12,19 +12,19 @@ class Word:
         self.word = word
         self.bestSuccession = []
     def getBestSuccession(self):
-        if(self.index == len(words)):
-            return
-        bestCandidate = [0,0]
         found = False
-        for i in range(self.index, len(words)):
-            if(len(words[i].bestSuccession) >= bestCandidate[1] and words[i].letters[0] == self.letters[1]):
-                bestCandidate = [i, len(words[i].bestSuccession)]
-                found = True
-        if(found):
-            self.bestSuccession = [bestCandidate[0]] + words[bestCandidate[0]].bestSuccession
+        if(self.index != len(words) - 1):
+            bestCandidate = [0,0]
+            for i in range(self.index + 1, len(words)):
+                if(len(words[i].bestSuccession) >= bestCandidate[1] and words[i].letters[0] == self.letters[1]):
+                    bestCandidate = [i, len(words[i].bestSuccession)]
+                    found = True
+            if(found):
+                self.bestSuccession = [bestCandidate[0]] + words[bestCandidate[0]].bestSuccession
         print(self.index, self.word, found, self.bestSuccession) 
-for i in range(0, len(file) - 1):
-    words.append(Word(file[i], i))
+
+for i in range(0, numberOfWords):
+    words.append(Word(file[i + 1], i))
 
 for e in range(len(words) - 1, -1, -1):
     words[e].getBestSuccession()
