dfcde221889c18d8e36e35d1fb81fd87ddf969bd
AnXPyDev <jkism332@gmail.com>
2018-11-22 06:10:30 +0100

    Started writing solution for problem 3 in LaTeX

---
+++ B/2/python/main.py
@@ -0,0 +1,35 @@
+from sys import argv
+
+file = open("./input/" + argv[1], "r").read().split('\n')
+
+words = []
+numberOfWords = int(file[0])
+
+class Word:
+    def __init__(self, word, index):
+        self.letters = [word[0], word[len(word) - 1]]
+        self.index = index
+        self.word = word
+        self.bestSuccession = []
+    def getBestSuccession(self):
+        found = False
+        if(self.index != len(words) - 1):
+            bestCandidate = [0,0]
+            for i in range(self.index + 1, len(words)):
+                if(len(words[i].bestSuccession) >= bestCandidate[1] and words[i].letters[0] == self.letters[1]):
+                    bestCandidate = [i, len(words[i].bestSuccession)]
+                    found = True
+            if(found):
+                self.bestSuccession = [bestCandidate[0]] + words[bestCandidate[0]].bestSuccession
+for i in range(0, numberOfWords):
+    words.append(Word(file[i + 1], i))
+
+for e in range(len(words) - 1, -1, -1):
+    words[e].getBestSuccession()
+
+max = 0
+for x in words:
+    if(len(x.bestSuccession) + 1 > max):
+        max = len(x.bestSuccession)
+
+print(max + 1)
