8bb9d7bf0667e10b6935af9f0ccb2b9300cefaa5
AnXPyDev <jkism332@gmail.com>
2020-11-08 22:03:54 +0100

    First version of 2020/A/3 completed

---
+++ 2020/A/3/py/.#sol.py
@@ -0,0 +1,1 @@
+anon@DESKTOP-E4UISSJ.762
--- 2020/A/3/py/sol.py
+++ 2020/A/3/py/sol.py
@@ -1,30 +1,33 @@
 n = 7
 k = 600
-stanky = [200, 100, 200, 300, 200, 200, 100]
+a = [200, 100, 200, 300, 200, 200, 100]
 
 vysledok_d = 0
 vysledok_l = 0
 
 l = 0
 r = n - 1
+p = sum(a)
 
-potrebne_k = sum(stanky)
-
-if potrebne_k < k:
+if p < k:
     vysledok_d = n
+    vysledok_l = 0
+elif k < min(a):
+    vysledok_d = 0
     vysledok_l = 0
 else:
     while l <= n:
-        while potrebne_k < k:
+        while p < k and r > l + 1:
             r += 1
-            potrebne_k += stanky[r]
-        while potrebne_k > k:
+            p += a[r]
+        while p > k and r + 1 < n:
+            p -= a[r]
             r -= 1
-            potrebne_k -= stanky[r]
         d = r - l
         if d > vysledok_d:
             vysledok_d = d
             vysledok_l = l
+        potrebne_k 
         l += 1
     
         
